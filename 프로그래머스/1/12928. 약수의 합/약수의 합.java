100ed6d305d2064428492034e0fa607aa62f6a3c
JiwonBlue <142164128+JiwonBlue@users.noreply.github.com>
2023-12-07 01:02:22 +0900

    [level 1] Title: 약수의 합, Time: 0.07 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(int i=1; i<3000; i++)
+        {
+            if((n!=0) && (n%i==0)) answer += i;
+        }
+        return answer;
+    }
+}
