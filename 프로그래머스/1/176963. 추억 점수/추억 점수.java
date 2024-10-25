88de7b2b7d9405782aec215149532c2928d4858d
JiwonBlue <142164128+JiwonBlue@users.noreply.github.com>
2024-10-25 11:17:53 +0900

    [level 1] Title: 추억 점수, Time: 3.40 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,15 +1,23 @@
+import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        for(int i=0; i<photo.length; i++){
+        HashMap<String,Integer> map = new LinkedHashMap<>();
+        for(int i=0; i< name.length; i++){
+            map.put(name[i],yearning[i]);
+        }
+
+        for(int i =0; i< photo.length;i++){
+            int score = 0;
             for(int j=0; j<photo[i].length; j++){
-                for(int k=0; k<name.length; k++){
-                    if(photo[i][j].equals(name[k])){
-                        answer[i] += yearning[k];
-                    }
+                if(map.containsKey(photo[i][j])){
+                    score+=map.get(photo[i][j]);
                 }
             }
+            answer[i]=score;
         }
+
         return answer;
+
     }
 }
